46dbfcf005217f0e10353fc332d1b8abae397711
Toby Murray <toby@ksu.edu>
2015-09-18 18:04:44 -0500

    Fixing type erasure problem
    The type of the list being passed into parseToList is being forgotten somewhere so this can't be abstracted

--- src/main/java/edu/ksu/canvas/impl/SectionsImpl.java
+++ src/main/java/edu/ksu/canvas/impl/SectionsImpl.java
@@ -1,14 +1,20 @@
 package edu.ksu.canvas.impl;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import edu.ksu.canvas.enums.SectionIncludes;
 import edu.ksu.canvas.interfaces.SectionReader;
@@ -18,23 +24,28 @@ import edu.ksu.canvas.util.CanvasURLBuilder;
 
 public class SectionsImpl extends BaseImpl implements SectionReader {
 
+    private static final Logger LOG = Logger.getLogger(SectionReader.class);
+
     public SectionsImpl(String canvasBaseUrl, Integer apiVersion, String oauthToken) {
         super(canvasBaseUrl, apiVersion, oauthToken);
     }
 
     @Override
     public List<Section> listCourseSections(Integer courseId, List<SectionIncludes> includes) throws IOException {
+        LOG.debug("Looking up sections for course " + courseId);
         ImmutableMap<String, List<String>> parameters = ImmutableMap.<String,List<String>>builder()
                 .put("include[]", includes.stream().map(Enum::name).collect(Collectors.toList()))
                 .build();
         String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion, "/courses/" + courseId + "/sections", parameters);
         List<Section> sections = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+        Type listType = new TypeToken<List<Section>>(){}.getType();
         while(StringUtils.isNotBlank(url)) {
             Response response = canvasMessenger.getFromCanvas(oauthToken, url);
             if (response.getErrorHappened() || response.getResponseCode() != 200) {
                 return Collections.emptyList();
             }
-            sections.addAll(responseParser.parseToList(Section.class, response));
+            sections.addAll(gson.fromJson(response.getContent(), listType));
             url = response.getNextLink();
         }
         return sections;
