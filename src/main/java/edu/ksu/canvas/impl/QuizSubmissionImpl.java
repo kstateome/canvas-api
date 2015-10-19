e78376a754cf69d702001f3a868e482e03439d7f
japshvincent@gmail.com <japshvincent@gmail.com>
2015-10-19 13:49:28 -0500

    added implementation from quizsubmission methods

--- src/main/java/edu/ksu/canvas/impl/QuizSubmissionImpl.java
+++ src/main/java/edu/ksu/canvas/impl/QuizSubmissionImpl.java
@@ -6,13 +6,17 @@ import edu.ksu.canvas.model.quizzes.QuizSubmission;
 import edu.ksu.canvas.net.Response;
 import edu.ksu.canvas.net.RestClient;
 import edu.ksu.canvas.util.CanvasURLBuilder;
+import edu.ksu.lti.error.MessageUndeliverableException;
 import edu.ksu.lti.error.OauthTokenRequiredException;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class QuizSubmissionImpl extends  BaseImpl implements QuizSubmissionReader, QuizSubmissionWriter {
@@ -29,6 +33,38 @@ public class QuizSubmissionImpl extends  BaseImpl implements QuizSubmissionReade
         List<Response> responses = canvasMessenger.getFromCanvas(oauthToken, url);
         return parseQuizSubmissionList(responses);
     }
+
+    @Override
+    public Optional<QuizSubmission> startQuizSubmission(String wid, String courseId, String quizId, String accessCode)
+            throws MessageUndeliverableException, IOException, OauthTokenRequiredException{
+        Map<String,String> postParams = new HashMap<>();
+        postParams.put("as_user_id", "sis_user_id:" + wid);
+        if(accessCode != null) {
+            postParams.put("access_code", accessCode);
+        }
+        String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion,
+                "courses/" + courseId + "/quizzes/" + quizId + "/submissions", Collections.emptyMap());
+        Response response = canvasMessenger.sendToCanvas(oauthToken, url,postParams);
+        return Optional.of(parseQuizSubmissionList(response).get(0));
+    }
+
+    @Override
+    public Optional<QuizSubmission> completeQuizSubmission(QuizSubmission submission, String wid, String courseId, String accessCode)
+            throws MessageUndeliverableException, IOException, OauthTokenRequiredException {
+        Map<String,String> postParams = new HashMap<>();
+        postParams.put("as_user_id", "sis_user_id:" + wid);
+        if(accessCode != null) {
+            postParams.put("access_code", accessCode);
+        }
+        String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion,
+                "courses/" + courseId + "/quizzes/" + submission.getQuiz_id() +
+                "/submissions/" + submission.getId() + "/complete",
+                Collections.emptyMap());
+        Response response = canvasMessenger.sendToCanvas(oauthToken, url,postParams);
+        return Optional.of(parseQuizSubmissionList(response).get(0));
+    }
+
+
 
     private List <QuizSubmission> parseQuizSubmissionList(final List<Response> responses) {
         return responses.stream().
--- src/main/java/edu/ksu/canvas/interfaces/QuizSubmissionWriter.java
+++ src/main/java/edu/ksu/canvas/interfaces/QuizSubmissionWriter.java
@@ -1,4 +1,15 @@
 package edu.ksu.canvas.interfaces;
 
+import edu.ksu.canvas.model.quizzes.QuizSubmission;
+import edu.ksu.lti.error.MessageUndeliverableException;
+import edu.ksu.lti.error.OauthTokenRequiredException;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public interface QuizSubmissionWriter {
+    Optional<QuizSubmission> startQuizSubmission(String wid, String courseId, String quizId, String accessCode)
+            throws MessageUndeliverableException, IOException, OauthTokenRequiredException;
+    Optional<QuizSubmission> completeQuizSubmission(QuizSubmission submission, String wid, String courseId, String accessCode)
+            throws MessageUndeliverableException, IOException, OauthTokenRequiredException;
 }
