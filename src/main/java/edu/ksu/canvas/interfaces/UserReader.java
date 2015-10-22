6c35c2f0ce7eea1e76d5c2afa8b84f73c4c9da4e
japshvincent@gmail.com <japshvincent@gmail.com>
2015-10-22 15:06:41 -0500

    added implementation and interface for getting user list from a course.

--- src/main/java/edu/ksu/canvas/interfaces/UserReader.java
+++ src/main/java/edu/ksu/canvas/interfaces/UserReader.java
@@ -1,5 +1,12 @@
 package edu.ksu.canvas.interfaces;
 
+import edu.ksu.canvas.exception.OauthTokenRequiredException;
+import edu.ksu.canvas.model.User;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface UserReader {
+    List<User> getUsersInCourse(String oauthToken, String courseId) throws OauthTokenRequiredException, IOException;
 
 }
