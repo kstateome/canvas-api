f9b169c5ab0f08c90dd720742f499973b5182934
Toby Murray <toby@ksu.edu>
2016-04-18 17:24:12 -0500

    Fixing DropCourseUTest

--- src/test/java/edu/ksu/canvas/tests/course/DropCourseUTest.java
+++ src/test/java/edu/ksu/canvas/tests/course/DropCourseUTest.java
@@ -2,7 +2,6 @@ package edu.ksu.canvas.tests.course;
 
 import edu.ksu.canvas.CanvasTestBase;
 import edu.ksu.canvas.impl.EnrollmentsImpl;
-import edu.ksu.canvas.interfaces.EnrollmentsReader;
 import edu.ksu.canvas.interfaces.EnrollmentsWriter;
 import edu.ksu.canvas.model.Enrollment;
 import edu.ksu.canvas.net.FakeRestClient;
@@ -12,24 +11,23 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 public class DropCourseUTest extends CanvasTestBase {
     @Autowired
     private FakeRestClient fakeRestClient;
     private EnrollmentsWriter enrollmentsWriter;
-    private EnrollmentsReader enrollmentsReader;
 
     @Before
     public void setupData() {
         enrollmentsWriter = new EnrollmentsImpl(baseUrl, apiVersion, oauthToken.getToken(), fakeRestClient);
-        enrollmentsReader = new EnrollmentsImpl(baseUrl, apiVersion, oauthToken.getToken(), fakeRestClient);
     }
 
     @Test
     public void testEnrollUser() throws IOException {
         //should make sure user exists
+        String url = baseUrl + "/api/v1/courses/25/enrollments";
+        fakeRestClient.addSuccessResponse(url, "SampleJson/EnrollmentResponse.json");
         Optional<Enrollment> enrollmentResponse = enrollmentsWriter.enrollUser(oauthToken.getToken(),25,78839);
         Assert.assertTrue(25==enrollmentResponse.get().getCourseId());
         Assert.assertTrue(78839 == enrollmentResponse.get().getUserId());
@@ -38,8 +36,8 @@ public class DropCourseUTest extends CanvasTestBase {
 
     @Test
     public void  testDropEnrolledUser() throws IOException {
-        //TODO get user enrollments and find enrollment id .
-        List<Enrollment> getEnrollments = enrollmentsReader.getUserEnrollments(oauthToken.getToken(), 78839);
+        String url = baseUrl + "/api/v1/courses/25/enrollments/355047";
+        fakeRestClient.addSuccessResponse(url, "SampleJson/EnrollmentDeleteResponse.json");
         Optional<Enrollment> dropResponse = enrollmentsWriter.dropUser(oauthToken.getToken(), 25, 355047L);
         Assert.assertTrue("deleted".equals(dropResponse.get().getEnrollmentState()));
     }
