2c1918f90d63ef478ff83c3741f82865dfeaa0ad
Toby Murray <toby@ksu.edu>
2016-04-20 11:33:49 -0500

    Fixing whitespace in new class

--- src/main/java/edu/ksu/canvas/exception/MessageUndeliverableException.java
+++ src/main/java/edu/ksu/canvas/exception/MessageUndeliverableException.java
@@ -1,19 +1,20 @@
 package edu.ksu.canvas.exception;
 
 public class MessageUndeliverableException extends RuntimeException {
-	
-	private static final long serialVersionUID = 1L;
 
-	private String message;
+    private static final long serialVersionUID = 1L;
 
-	public MessageUndeliverableException(String message) {
+    private String message;
+
+    public MessageUndeliverableException(String message) {
         this.setMessage(message);
     }
 
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
