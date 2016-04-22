4dc36214da5fe8cf5cbe13c222f158180e689246
Killian Stone <killsto@ksu.edu>
2016-04-21 19:19:52 -0500

    Added generation of POST maps.
    I added functionallity to generate POST's parametters to canvas from our
    canvas models. This was done by creating 2 annotations. Still need to
    add the annotations to the models

---
+++ src/main/java/edu/ksu/canvas/model/BaseCanvasModel.java
@@ -0,0 +1,45 @@
+package edu.ksu.canvas.model;
+
+import edu.ksu.canvas.annotation.CanvasField;
+import edu.ksu.canvas.annotation.CanvasObject;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class BaseCanvasModel {
+    /* Canvas has post parameter keys in non consistent formats. Occasionally they are 'class[field]' and other times
+     * they are just 'field'. This method will create a map with the correct post keys and values based on the
+     * @CanvasField and @CanvasObject annotations.
+     */
+    public Map<String, Object> toPostMap() throws IllegalAccessException, InvocationTargetException {
+        Class<? extends BaseCanvasModel> clazz = this.getClass();
+        Map<String, Object> postMap = new HashMap<>();
+        for (Method method : clazz.getMethods()) {
+            CanvasField canvasFieldAnnotation = method.getAnnotation(CanvasField.class);
+            if (canvasFieldAnnotation != null && canvasFieldAnnotation.postKey() != null) {
+                String postKey = getPostKey(canvasFieldAnnotation);
+                Object value = method.invoke(this, null);
+                postMap.put(postKey, value);
+            }
+        }
+        return postMap;
+    }
+
+    private String getPostKey(CanvasField canvasFieldAnnotation) {
+        if (!canvasFieldAnnotation.array()) {
+            return canvasFieldAnnotation.postKey();
+        } else {
+            return makeArrayPostKey(canvasFieldAnnotation);
+        }
+    }
+
+    private String makeArrayPostKey(CanvasField canvasFieldAnnotation) {
+        CanvasObject canvasObjectAnnotation = this.getClass().getAnnotation(CanvasObject.class);
+        if (canvasObjectAnnotation == null || canvasObjectAnnotation.postKey() == null) {
+            throw new IllegalArgumentException("CanvasObject does not contain postKey for " + this.getClass().getName());
+        }
+        return canvasObjectAnnotation.postKey() + "[" + canvasFieldAnnotation.postKey() + "]";
+    }
+}
---
+++ src/test/java/edu/ksu/canvas/BaseCanvasModelUTest.java
@@ -0,0 +1,43 @@
+package edu.ksu.canvas;
+
+import edu.ksu.canvas.model.TestCanvasModel;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+public class BaseCanvasModelUTest {
+    public static final String CLASS_POST_KEY = "test";
+    public static final String FIELD1_POST_KEY = "field1Key";
+    public static final String FIELD2_POST_KEY = "field2Key";
+    public static final String FIELD1_VALUE = "field1Value";
+    public static final String FIELD2_VALUE = "field2Value";
+
+
+    /* Make sure canvasFields with arrays = true are of form object[field] */
+    @Test
+    public void postKeysAsArraysSetCorrectly() throws Exception {
+        String expectedKey = CLASS_POST_KEY + "[" + FIELD1_POST_KEY + "]";
+        TestCanvasModel canvasModel = new TestCanvasModel();
+        canvasModel.setArrayPostKey(FIELD1_VALUE);
+
+        Map<String, Object> postMap = canvasModel.toPostMap();
+
+        Assert.assertTrue("PostKey from canvasModel.toPostMap() did not have expected key for field1", postMap.containsKey(expectedKey));
+        Assert.assertEquals("Field1Value from canvasModel.toPostMap() did not have expected value for field1", FIELD1_VALUE, postMap.get(expectedKey));
+    }
+
+    /* Make sure canvasFields with arrays = false are of form object[field] */
+    @Test
+    public void postKeysAsNonArraysSetCorrectly() throws Exception {
+        String expectedKey = FIELD2_POST_KEY;
+        TestCanvasModel canvasModel = new TestCanvasModel();
+        canvasModel.setNonArrayPostKey(FIELD2_VALUE);
+
+        Map<String, Object> postMap = canvasModel.toPostMap();
+
+        Assert.assertTrue("PostKey from canvasModel.toPostMap() did not have expected key for field2", postMap.containsKey(expectedKey));
+        Assert.assertEquals("Field2Value from canvasModel.toPostMap() did not have expected value for field2", FIELD2_VALUE, postMap.get(expectedKey));
+    }
+
+}
