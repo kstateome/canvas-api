3cd7dabcfa06a015412260022a2a020d68ccd372
alexanda <alexanda@k-state.edu>
2016-04-22 14:47:50 -0500

    added annotations to Section class

--- src/main/java/edu/ksu/canvas/model/Section.java
+++ src/main/java/edu/ksu/canvas/model/Section.java
@@ -1,5 +1,8 @@
 package edu.ksu.canvas.model;
 
+import edu.ksu.canvas.annotation.CanvasField;
+import edu.ksu.canvas.annotation.CanvasObject;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -7,6 +10,7 @@ import java.util.Date;
  * Class to represent Canvas course sections.
  * See <a href="https://canvas.instructure.com/doc/api/sections.html#Section">Canvas Section</a> documentation.
  */
+@CanvasObject(postKey = "course_section")
 public class Section implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -30,6 +34,7 @@ public class Section implements Serializable {
         this.id = id;
     }
 
+    @CanvasField(postKey = "name")
     public String getName() {
         return name;
     }
@@ -38,6 +43,7 @@ public class Section implements Serializable {
         this.name = name;
     }
 
+    @CanvasField(postKey = "sis_section_id")
     public String getSisSectionId() {
         return sisSectionId;
     }
@@ -78,6 +84,7 @@ public class Section implements Serializable {
         this.sisCourseId = sisCourseId;
     }
 
+    @CanvasField(postKey = "start_at")
     public Date getStartAt() {
         return startAt;
     }
@@ -86,6 +93,7 @@ public class Section implements Serializable {
         this.startAt = startAt;
     }
 
+    @CanvasField(postKey = "end_at")
     public Date getEndAt() {
         return endAt;
     }
