4530423ee3a1d002edafd1ee0b8556de86ac3e56
japshvincent <japshvincent@k-state.edu>
2016-04-28 10:35:27 -0500

    changes for User read operations to make use of masquerading using sis_id or canvas_id

--- src/main/java/edu/ksu/canvas/interfaces/UserWriter.java
+++ src/main/java/edu/ksu/canvas/interfaces/UserWriter.java
@@ -6,7 +6,7 @@ import edu.ksu.canvas.model.User;
 import java.io.IOException;
 import java.util.Optional;
 
-public interface UserWriter extends CanvasWriter {
+public interface UserWriter extends CanvasWriter<User, CanvasWriter> {
     /**
      *
      * @param user  user data for creating user account
