232f0224692134dc87b44ae734685b6eda22e6bb
Toby Murray <toby@ksu.edu>
2016-04-28 18:46:27 -0500

    Adding a method for getting users in a course
    This commit causes some unit tests to fail however I believe the problem is actually in the BaseImpl buildCanvasUrl method.
    It is not handling empty includes/enrollment type lists correctly when it comes to masquerading.

--- src/main/java/edu/ksu/canvas/interfaces/UserReader.java
+++ src/main/java/edu/ksu/canvas/interfaces/UserReader.java
@@ -1,11 +1,24 @@
 package edu.ksu.canvas.interfaces;
 
+import edu.ksu.canvas.enums.CourseIncludes;
+import edu.ksu.canvas.enums.EnrollmentType;
 import edu.ksu.canvas.exception.OauthTokenRequiredException;
 import edu.ksu.canvas.model.User;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserReader extends CanvasReader<User, UserReader> {
-    List<User> getUsersInCourse(String courseId) throws OauthTokenRequiredException, IOException;
+    /**
+     * Retrieve a list of users in a course
+     * @param courseId Required. Course ID of course to query.
+     * @param enrollmentTypes Optional list of enrollment types to include. An empty list will not restrict by enrollment type.
+     * @param enrollmentRoleId Optional role ID which will restrict the returned users to only the given role
+     * @param includes Optional list of extra information to include in the returned user objects
+     * @return List of users in a course
+     * @throws OauthTokenRequiredException
+     * @throws IOException
+     */
+    List<User> getUsersInCourse(String courseId, List<EnrollmentType> enrollmentTypes, Optional<Integer> enrollmentRoleId, List<CourseIncludes> includes) throws OauthTokenRequiredException, IOException;
 }
