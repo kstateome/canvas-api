22c4f9fb985351858b6144c47708857cabcda2de
japshvincent <japshvincent@k-state.edu>
2016-04-28 10:00:44 -0500

    changes for Quiz read operations to make use of masquerading using sis_id or canvas_id

--- src/main/java/edu/ksu/canvas/impl/QuizImpl.java
+++ src/main/java/edu/ksu/canvas/impl/QuizImpl.java
@@ -28,24 +28,21 @@ public class QuizImpl extends BaseImpl<Quiz, QuizReader> implements QuizReader, 
 
     @Override
     public Optional<Quiz> getSingleQuiz(String courseId, String quizId) throws OauthTokenRequiredException, IOException {
-        String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion,
-                "courses/" + courseId + "/quizzes/" + quizId, Collections.emptyMap());
+        String url = buildCanvasUrl("courses/" + courseId + "/quizzes/" + quizId, Collections.emptyMap());
         Response response = canvasMessenger.getSingleResponseFromCanvas(oauthToken, url);
         return responseParser.parseToObject(Quiz.class, response);
     }
 
     @Override
     public List<Quiz> getQuizzesInCourse(String courseId) throws OauthTokenRequiredException, IOException {
-        String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion,
-                "courses/" + courseId + "/quizzes", Collections.emptyMap());
+        String url = buildCanvasUrl("courses/" + courseId + "/quizzes", Collections.emptyMap());
         List<Response> responses = canvasMessenger.getFromCanvas(oauthToken, url);
         return parseQuizList(responses);
     }
 
     @Override
     public Optional<Quiz> updateQuiz(Quiz quiz, String courseId) throws MessageUndeliverableException, IOException, OauthTokenRequiredException {
-        String url = CanvasURLBuilder.buildCanvasUrl(canvasBaseUrl, apiVersion,
-                "courses/" + courseId + "/quizzes/" + quiz.getId(), Collections.emptyMap());
+        String url = buildCanvasUrl("courses/" + courseId + "/quizzes/" + quiz.getId(), Collections.emptyMap());
         Response response = canvasMessenger.sendToJsonCanvas(oauthToken, url,
                 getDefaultGsonParser().toJsonTree(quiz).getAsJsonObject());
         return responseParser.parseToObject(Quiz.class, response);
