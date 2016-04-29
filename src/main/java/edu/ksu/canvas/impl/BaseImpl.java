5c586110234c6e1f3b8da244ce9b9c77fe64a792
Toby Murray <toby@ksu.edu>
2016-04-29 13:20:09 -0500

    Removing unused imports

--- src/main/java/edu/ksu/canvas/impl/BaseImpl.java
+++ src/main/java/edu/ksu/canvas/impl/BaseImpl.java
@@ -20,8 +20,6 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
