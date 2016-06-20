33e98aa3366c7b787c110fd561373af4d1b83604
David Meierer <dmeierer@ksu.edu>
2016-06-20 09:31:15 -0500

    Initial work on implementing a put method in api library.

--- src/test/java/edu/ksu/canvas/net/FakeRestClient.java
+++ src/test/java/edu/ksu/canvas/net/FakeRestClient.java
@@ -42,6 +42,13 @@ public class FakeRestClient implements RestClient {
         return response(url);
     }
 
+    @Override
+    public Response sendApiPut(@NotNull String token, @NotNull String url, Map<String, String> putParameters, int connectTimeout, int readTimeout) throws InvalidOauthTokenException, IOException {
+        LOG.debug("Sending fake PUT to " + url);
+        checkForTimeout(connectTimeout, readTimeout);
+        return response(url);
+    }
+
     private void checkForTimeout(int connectTimeout, int readTimeout) throws IOException {
         if (connectTimeout > NO_TIMEOUT) {
             throw new IOException("Connect timeout exceeded");
