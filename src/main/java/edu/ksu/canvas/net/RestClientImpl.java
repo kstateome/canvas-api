6e59ed882559ebfe5e6a9d121ad6a026ab6811af
David Meierer <dmeierer@ksu.edu>
2016-06-20 10:50:25 -0500

    Initial work to make use of the apache http client for the other http methods.

--- src/main/java/edu/ksu/canvas/net/RestClientImpl.java
+++ src/main/java/edu/ksu/canvas/net/RestClientImpl.java
@@ -1,13 +1,19 @@
 package edu.ksu.canvas.net;
 
 import edu.ksu.canvas.exception.InvalidOauthTokenException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 
 import javax.validation.constraints.NotNull;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
@@ -19,31 +25,29 @@ public class RestClientImpl implements RestClient {
     private static final Logger LOG = Logger.getLogger(RestClientImpl.class);
 
     public Response sendApiGet(@NotNull String token, @NotNull String url,
-                                      int connectTimeout, int readTimeout) throws IOException {
+                               int connectTimeout, int readTimeout) throws IOException {
         LOG.debug("url - " + url);
         Long beginTime = System.currentTimeMillis();
         Response response = new Response();
-        URL apiUrl = new URL(url);
-        HttpURLConnection con = (HttpURLConnection)apiUrl.openConnection();
-        con.setConnectTimeout(connectTimeout);
-        con.setReadTimeout(readTimeout);
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Authorization", "Bearer" + " " + token);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setHeader("Authorization", "Bearer" + " " + token);
 
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
         //deal with the actual content
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
         String inputLine;
         StringBuffer content = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
         response.setContent(content.toString());
-        response.setResponseCode(con.getResponseCode());
+        response.setResponseCode(httpResponse.getStatusLine().getStatusCode());
         Long endTime = System.currentTimeMillis();
         LOG.debug("Canvas API call took: " + (endTime - beginTime) + "ms");
 
         //deal with pagination
-        String linkHeader = con.getHeaderField("Link");
+        String linkHeader = httpResponse.getFirstHeader("Link").getValue();
         if(linkHeader == null) {
             return response;
         }
@@ -60,31 +64,25 @@ public class RestClientImpl implements RestClient {
 
     //TODO: remove awful duplication
     public Response sendJsonPost(String token, String url, String json,
-                                        int connectTimeout, int readTimeout) throws IOException {
+                                 int connectTimeout, int readTimeout) throws IOException {
         LOG.debug("sendApiPost");
         Response response = new Response();
-
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
         Long beginTime = System.currentTimeMillis();
-        URL apiUrl = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) apiUrl.openConnection();
-        con.setConnectTimeout(connectTimeout);
-        con.setReadTimeout(readTimeout);
-        con.setDoOutput(true);
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Authorization", "Bearer" + " " + token);
-        con.setRequestProperty("Content-Type", "application/json");
+        httpPost.setHeader("Authorization", "Bearer" + " " + token);
+        httpPost.setHeader("Content-Type", "application/json");
 
-        OutputStream outputStream = con.getOutputStream();
-        outputStream.write(json.getBytes());
-        outputStream.close();
-
+        StringEntity params = new StringEntity(json);
+        httpPost.setEntity(params);
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
         LOG.debug("Sending API POST request to URL: " + url);
-        if (con.getResponseCode() == 401) {
+        if (httpResponse.getStatusLine().getStatusCode() == 401) {
             throw new InvalidOauthTokenException();
         }
 
         BufferedReader in = new BufferedReader(new InputStreamReader(
-                con.getInputStream()));
+                httpPost.getEntity().getContent()));
         String inputLine;
         StringBuffer content = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
@@ -93,20 +91,6 @@ public class RestClientImpl implements RestClient {
         response.setContent(content.toString());
         Long endTime = System.currentTimeMillis();
         LOG.debug("Canvas API call took: " + (endTime - beginTime) + "ms");
-
-        //deal with pagination
-        String linkHeader = con.getHeaderField("Link");
-        if (linkHeader == null) {
-            return response;
-        }
-        List<String> links = Arrays.asList(linkHeader.split(","));
-        for (String link : links) {
-            if (link.contains("rel=\"next\"")) {
-                LOG.debug("response has more pages");
-                String nextLink = link.substring(1, link.indexOf(';')); //format is <http://.....>; rel="next"
-                response.setNextLink(nextLink);
-            }
-        }
         return response;
     }
 
@@ -161,55 +145,38 @@ public class RestClientImpl implements RestClient {
         return response;
     }
 
-
-
     public Response sendApiDelete(String token, String url,Map<String, String> deleteParameters,
-                                       int connectTimeout, int readTimeout) throws InvalidOauthTokenException, IOException {
-        LOG.debug("sendApiPost");
+                                  int connectTimeout, int readTimeout) throws InvalidOauthTokenException, IOException {
+        LOG.debug("sendApiDelete");
         Response response = new Response();
 
         Long beginTime = System.currentTimeMillis();
-        URL apiUrl = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) apiUrl.openConnection();
-        con.setConnectTimeout(connectTimeout);
-        con.setReadTimeout(readTimeout);
-        con.setRequestMethod("DELETE");
-        con.setRequestProperty("Authorization", "Bearer" + " " + token);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpDelete httpDelete = new HttpDelete(url);
+        httpDelete.setHeader("Authorization", "Bearer" + " " + token);
         if (deleteParameters != null) {
             for (Map.Entry<String, String> entry : deleteParameters.entrySet()) {
-                con.setRequestProperty(entry.getKey(), entry.getValue());
+                httpDelete.addHeader(entry.getKey(), entry.getValue());
             }
         }
+
+        CloseableHttpResponse httpResponse = httpClient.execute(httpDelete);
         LOG.debug("Sending API DELETE request to URL: " + url);
-        if (con.getResponseCode() == 401) {
+        if (httpResponse.getStatusLine().getStatusCode() == 401) {
             throw new InvalidOauthTokenException();
         }
 
         BufferedReader in = new BufferedReader(new InputStreamReader(
-                con.getInputStream()));
+                httpResponse.getEntity().getContent()));
         String inputLine;
         StringBuffer content = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
             content.append(inputLine);
         }
         response.setContent(content.toString());
-        response.setResponseCode(con.getResponseCode());
+        response.setResponseCode(httpResponse.getStatusLine().getStatusCode());
         Long endTime = System.currentTimeMillis();
         LOG.debug("Canvas API call took: " + (endTime - beginTime) + "ms");
-
-        //deal with pagination
-        String linkHeader = con.getHeaderField("Link");
-        if (linkHeader == null) {
-            return response;
-        }
-        List<String> links = Arrays.asList(linkHeader.split(","));
-        for (String link : links) {
-            if (link.contains("rel=\"next\"")) {
-                LOG.debug("response has more pages");
-                String nextLink = link.substring(1, link.indexOf(';')); //format is <http://.....>; rel="next"
-                response.setNextLink(nextLink);
-            }
-        }
         return response;
     }
 }
