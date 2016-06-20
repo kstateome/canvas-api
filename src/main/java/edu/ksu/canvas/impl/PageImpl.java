fc0ad492b5199c638230c3aa68042e283487beca
Toby Murray <toby@ksu.edu>
2016-06-20 10:57:20 -0500

    Adding a method to push course page changes to Canvas

--- src/main/java/edu/ksu/canvas/impl/PageImpl.java
+++ src/main/java/edu/ksu/canvas/impl/PageImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.apache.log4j.Logger;
@@ -36,6 +37,14 @@ public class PageImpl extends BaseImpl<Page, PageReader, PageWriter> implements 
         LOG.debug("retrieving page " + pageUrl + " for group " + groupId);
         String url = buildCanvasUrl("groups/" + groupId + "/pages/" + pageUrl, Collections.emptyMap());
         Response response = canvasMessenger.getSingleResponseFromCanvas(oauthToken, url);
+        return responseParser.parseToObject(Page.class, response);
+    }
+
+    @Override
+    public Optional<Page> updateCoursePage(Page page, Integer courseId) throws Exception {
+        LOG.debug("Updating page in course" + courseId);
+        String url = buildCanvasUrl("courses/" + courseId + "/pages/" + page.getUrl(), Collections.emptyMap());
+        Response response = canvasMessenger.putToCanvas(oauthToken, url, page.toPostMap());
         return responseParser.parseToObject(Page.class, response);
     }
 
--- src/main/java/edu/ksu/canvas/interfaces/PageWriter.java
+++ src/main/java/edu/ksu/canvas/interfaces/PageWriter.java
@@ -6,4 +6,12 @@ import edu.ksu.canvas.model.Page;
 
 public interface PageWriter extends CanvasWriter<Page, PageWriter> {
 
+    /**
+     * Save a course page to Canvas
+     * @param page The page object to save
+     * @param courseId The Canvas course ID that this page is to be saved to
+     * @return The update page after saving
+     * @throws Exception
+     */
+    Optional<Page> updateCoursePage(Page page, Integer courseId) throws Exception;
 }
