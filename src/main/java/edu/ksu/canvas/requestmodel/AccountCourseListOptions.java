2afe9bd4be33ebe3db295c58c83f1272b458bc48
Toby Murray <toby@ksu.edu>
2016-06-30 19:30:02 -0500

    Fleshing out all the options for the account list API call

--- src/main/java/edu/ksu/canvas/requestmodel/AccountCourseListOptions.java
+++ src/main/java/edu/ksu/canvas/requestmodel/AccountCourseListOptions.java
@@ -1,13 +1,72 @@
 package edu.ksu.canvas.requestmodel;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AccountCourseListOptions extends BaseOptions {
 
     private String accountId;
+    public enum EnrollmentType {
+        student, teacher, ta, observer, designer;
+    }
+
+    public enum State {
+        created, claimed, available, completed, deleted, all;
+    }
+
+    public enum Includes {
+        syllabus_body, term, course_progress, storage_quota_used_mb, total_students, teachers;
+    }
 
     public AccountCourseListOptions(String accountId) {
         this.accountId = accountId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public AccountCourseListOptions withEnrollments(Boolean enrollments) {
+        optionsMap.put("with_enrollments", Arrays.asList(enrollments.toString()));
+        return this;
+    }
+
+    public AccountCourseListOptions withEnrollmentTypes(List<EnrollmentType> enrollmentTypes) {
+        List<String> enrollmentStrings = enrollmentTypes.stream().map(e -> e.name()).collect(Collectors.toList());
+        optionsMap.put("enrollment_type[]", enrollmentStrings);
+        return this;
+    }
+
+    public AccountCourseListOptions onlyPublished(Boolean published) {
+        optionsMap.put("published", Arrays.asList(published.toString()));
+        return this;
+    }
+
+    public AccountCourseListOptions onlyCompleted(Boolean completed) {
+        optionsMap.put("published", Arrays.asList(completed.toString()));
+        return this;
+    }
+
+    public AccountCourseListOptions taughtByTeachers(List<String> teacherList) {
+        optionsMap.put("by_teachers[]", teacherList);
+        return this;
+    }
+
+    public AccountCourseListOptions inSubaccount(List<String> accountList) {
+        optionsMap.put("by_subaccounts[]", accountList);
+        return this;
+    }
+
+    public AccountCourseListOptions hideEnrollmentlessCourses(Boolean hide) {
+        optionsMap.put("hide_enrollmentless_courses", Arrays.asList(hide.toString()));
+        return this;
+    }
+
+    public AccountCourseListOptions inState(List<State> stateList) {
+        List<String> stateStrings = stateList.stream().map(s -> s.name()).collect(Collectors.toList());
+        optionsMap.put("state[]", stateStrings);
+        return this;
     }
 
     public AccountCourseListOptions addEnrollmentTermId(String enrollmentTermId) {
@@ -15,7 +74,14 @@ public class AccountCourseListOptions extends BaseOptions {
         return this;
     }
 
-    public String getAccountId() {
-        return accountId;
+    public AccountCourseListOptions searchTerm(String searchTerm) {
+        optionsMap.put("search_term", Arrays.asList(searchTerm));
+        return this;
+    }
+
+    public AccountCourseListOptions includes(List<Includes> includes) {
+        List<String> includeStrings = includes.stream().map(i -> i.name()).collect(Collectors.toList());
+        optionsMap.put("include[]", includeStrings);
+        return this;
     }
 }
