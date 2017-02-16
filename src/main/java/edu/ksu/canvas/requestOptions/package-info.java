904e8987f2459495fcc39f8a2a8d8367dd6354d6
Toby Murray <toby@ksu.edu>
2017-02-16 10:43:07 -0600

    Adding a package info file for request options

---
+++ src/main/java/edu/ksu/canvas/requestOptions/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes to encapsulate parameters that get passed to API calls.
+ * <p>
+ * Some API calls take a huge list of (often optional) parameters. The
+ * classes in this package allow type-safe usage of these parameters
+ * and allow new parameters to be added without breaking existing code.
+ */
+package edu.ksu.canvas.requestOptions;
