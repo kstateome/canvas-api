d6597f7151367f2f4a14921e63bb98a26e508512
Toby Murray <toby@ksu.edu>
2017-02-16 10:23:51 -0600

    Added a missing field in the error response object
    Also did some cleanup in exceptions.

--- src/main/java/edu/ksu/canvas/model/status/package-info.java
+++ src/main/java/edu/ksu/canvas/model/status/package-info.java
@@ -1,6 +1,8 @@
 /**
- * Objects to represent responses from the Canvas API that don't represent
- * actual objects in Canvas but rather status responses such as error
- * messages or success/failure type responses.
+ * Objects to represent Canvas status responses.
+ * <p>
+ * Some API calls do not return Canvas domain objects but rather
+ * a success/failure status or an error message. This package
+ * contains objects to parse these responses.
  */
 package edu.ksu.canvas.model.status;
