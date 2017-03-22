3e43fcd818d197e50893c66ecaa6b92880c93f9d
dmeierer <dmeierer@ksu.edu>
2017-03-22 10:08:24 -0500

    More tweaking plus added some comments to wrapper class.

--- src/main/java/edu/ksu/canvas/impl/EnrollmentTermImpl.java
+++ src/main/java/edu/ksu/canvas/impl/EnrollmentTermImpl.java
@@ -1,7 +1,6 @@
 package edu.ksu.canvas.impl;
 
 
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import edu.ksu.canvas.interfaces.EnrollmentTermReader;
 import edu.ksu.canvas.interfaces.EnrollmentTermWriter;
@@ -15,7 +14,9 @@ import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnrollmentTermImpl extends BaseImpl<EnrollmentTerm, EnrollmentTermReader, EnrollmentTermWriter> implements EnrollmentTermReader, EnrollmentTermWriter {
     private static final Logger LOG = Logger.getLogger(EnrollmentTermReader.class);
@@ -29,8 +30,18 @@ public class EnrollmentTermImpl extends BaseImpl<EnrollmentTerm, EnrollmentTermR
         LOG.debug("getting enrollment term with account id " + options.getAccountId());
         String url = buildCanvasUrl("accounts/" + options.getAccountId() + "/terms/" , options.getOptionsMap());
         LOG.debug("Final URL of API call: " + url);
-        JsonObject requestBody = new JsonObject();
-        Response response = canvasMessenger.sendJsonPostToCanvas(oauthToken, url, requestBody);
+        List<Response> response = canvasMessenger.getFromCanvas(oauthToken, url);
+        return parseEnrollmentTermList(response);
+    }
+
+    private List<EnrollmentTerm> parseEnrollmentTermList(final List<Response> responses) {
+        return responses.stream().
+                map(this::parseEnrollmentTermList).
+                flatMap(Collection::stream).
+                collect(Collectors.toList());
+    }
+
+    private List<EnrollmentTerm> parseEnrollmentTermList(final Response response) {
         EnrollmentTermWrapper wrapper = GsonResponseParser.getDefaultGsonParser().fromJson(response.getContent(), EnrollmentTermWrapper.class);
         return wrapper.getEnrollmentTerms();
     }
--- src/main/java/edu/ksu/canvas/model/EnrollmentTermWrapper.java
+++ src/main/java/edu/ksu/canvas/model/EnrollmentTermWrapper.java
@@ -3,6 +3,10 @@ package edu.ksu.canvas.model;
 
 import java.util.List;
 
+/**
+ * Wrapper class made necessary because canvas returns an object that
+ * contains a list of EnrollmentTerm Objects, with only one item in the list.
+ */
 public class EnrollmentTermWrapper {
 
     private List<EnrollmentTerm> enrollmentTerms;
