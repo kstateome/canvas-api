d21cb55d11ceb9dd0af1b8c18af5df40799e7a66
John Callaway <John.Callaway@colorado.edu>
2017-08-15 09:26:07 -0600

    Modify delete call so it returns a Section instead of a boolean, and add the enableSisReactivation parameter to the create call.

--- src/main/java/edu/ksu/canvas/impl/SectionsImpl.java
+++ src/main/java/edu/ksu/canvas/impl/SectionsImpl.java
@@ -18,6 +18,7 @@ import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -59,23 +60,21 @@ public class SectionsImpl extends BaseImpl<Section, SectionReader, SectionWriter
     }
 
     @Override
-    public Optional<Section> createSection(String courseId, Section section) throws IOException {
+    public Optional<Section> createSection(String courseId, Section section, boolean enableSisReactivation)
+            throws IOException {
         LOG.debug("creating section for course " + courseId);
+        Map<String, String> params = new HashMap<>();
+        params.put("enable_sis_reactivation", Boolean.toString(enableSisReactivation));
         String url = buildCanvasUrl(String.format("/courses/%s/sections", courseId), Collections.emptyMap());
         Response response = canvasMessenger.sendJsonPostToCanvas(oauthToken, url, section.toJsonObject());
         return responseParser.parseToObject(Section.class, response);
     }
 
     @Override
-    public Boolean deleteSection(String sectionId) throws IOException {
+    public Optional<Section> deleteSection(String sectionId) throws IOException {
         LOG.debug("deleting section " + sectionId);
         String url = buildCanvasUrl("/sections/" + sectionId, Collections.emptyMap());
         Response response = canvasMessenger.deleteFromCanvas(oauthToken, url, Collections.emptyMap());
-        if (response.getErrorHappened() || response.getResponseCode() != 200) {
-            LOG.debug(String.format("Failed to delete section %s, error message: %s", sectionId, response.toString()));
-            return false;
-        }
-        Optional<Delete> responseParsed = responseParser.parseToObject(Delete.class, response);
-        return responseParsed.map(r -> r.getDelete()).orElse(false);
+        return responseParser.parseToObject(Section.class, response);
     }
 }
--- src/main/java/edu/ksu/canvas/interfaces/SectionWriter.java
+++ src/main/java/edu/ksu/canvas/interfaces/SectionWriter.java
@@ -16,17 +16,20 @@ public interface SectionWriter extends CanvasWriter<Section, SectionWriter> {
      * @param courseId identifies the parent course we are adding this section to
      * @param section a section object containing the information needed to create
      *         the section in Canvas
+     * @param enableSisReactivation when true, will first try to re-activate a
+     *         deleted section with matching sis_section_id if possible
      * @return the newly created section
      * @throws IOException
      */
-    Optional<Section> createSection(String courseId, Section section) throws IOException;
+    Optional<Section> createSection(String courseId, Section section, boolean enableSisReactivation) throws IOException;
 
     /**
      * Delete an existing section in Canvas.
      *
      * @param sectionId identifies the section to delete
      * @return true if the delete was successful
+     * @return the section object that was deleted
      * @throws IOException
      */
-    Boolean deleteSection(String sectionId) throws IOException;
+    Optional<Section> deleteSection(String sectionId) throws IOException;
 }
