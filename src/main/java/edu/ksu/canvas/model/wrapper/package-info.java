85371e3d36aeedceb773cf0d47b76a098bbdddc9
Toby Murray <toby@ksu.edu>
2017-10-30 12:11:17 -0500

    Moving wrapper objects into their own package
    These are just convenience objects for Gson parsing of API calls that return an object with a list instead of just the plain list.

---
+++ src/main/java/edu/ksu/canvas/model/wrapper/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Wrapper objects for when the Canvas API calls
+ * <p>
+ * Some API calls return an object with a list inside of it
+ * instead of just the list. The classes in here are just
+ * objects to hold lists for easy parsing via Gson. They
+ * should only be used internally to the API library.
+ */
+package edu.ksu.canvas.model.wrapper;
