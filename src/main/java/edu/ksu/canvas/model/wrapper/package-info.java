b276405351c834a4be6d8574dbe2f01207df64db
Toby Murray <toby@ksu.edu>
2017-12-12 10:24:54 -0600

    Fixing incomplete sentence in Javadoc

--- src/main/java/edu/ksu/canvas/model/wrapper/package-info.java
+++ src/main/java/edu/ksu/canvas/model/wrapper/package-info.java
@@ -1,9 +1,8 @@
 /**
- * Wrapper objects for when the Canvas API calls
+ * Wrapper objects for when the Canvas API returns lists wrapped in an object
  * <p>
- * Some API calls return an object with a list inside of it
- * instead of just the list. The classes in here are just
- * objects to hold lists for easy parsing via Gson. They
- * should only be used internally to the API library.
+ * Some API calls return an object with a list inside of it instead of just
+ * the list. The classes in here are just objects to hold lists for easy
+ * parsing via Gson. They should only be used internally to the API library.
  */
 package edu.ksu.canvas.model.wrapper;
