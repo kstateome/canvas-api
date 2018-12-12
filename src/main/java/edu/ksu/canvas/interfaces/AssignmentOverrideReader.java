66994f19b4e46a5276f97426a2314ccff91b518c
Toby Murray <toby@ksu.edu>
2018-12-12 17:27:57 -0600

    Added some missing javadoc descriptions

--- src/main/java/edu/ksu/canvas/interfaces/AssignmentOverrideReader.java
+++ src/main/java/edu/ksu/canvas/interfaces/AssignmentOverrideReader.java
@@ -9,20 +9,20 @@ public interface AssignmentOverrideReader extends CanvasReader<AssignmentOverrid
     
     /***
      * Returns the list of overrides for this assignment that target sections/groups/students visible to the current user.
-     * @param courseId
-     * @param assignmentId
-     * @return
-     * @throws IOException 
+     * @param courseId Canvas course ID or "sis_course_id:1234" for SIS course ID
+     * @param assignmentId Canvas assignment ID
+     * @return List of assignment overrides
+     * @throws IOException When there is an error communicating with Canvas
      */
     List<AssignmentOverride> listAssignmentOverrides(String courseId, Integer assignmentId) throws IOException;
     
     /***
      * Returns details of the the override with the given id.
-     * @param courseId
-     * @param assignmentId
-     * @param id
-     * @return
-     * @throws IOException 
+     * @param courseId Canvas course ID or "sis_course_id:1234" for SIS course ID
+     * @param assignmentId Canvas assignment ID
+     * @param id Canvas ID of the specific assignment override to query for
+     * @return The requested assigment override object
+     * @throws IOException When there is an error communicating with Canvas
      */
     Optional<AssignmentOverride> getAssignmentOverride(String courseId, Integer assignmentId, Integer id) throws IOException;
 }
