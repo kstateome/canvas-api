4356293a7a93bcf5cae014eac200f0b83395cbaa
Garrett <gnixsoftware@gmail.com>
2023-01-25 15:07:06 -0800

    Add Javadoc comments for DiscussionTopicReader

--- src/main/java/edu/ksu/canvas/interfaces/DiscussionTopicReader.java
+++ src/main/java/edu/ksu/canvas/interfaces/DiscussionTopicReader.java
@@ -10,8 +10,20 @@ import java.util.Optional;
 
 public interface DiscussionTopicReader extends CanvasReader<DiscussionTopic, DiscussionTopicReader> {
 
+	/**
+	 * Retireve a specific Discussion Topic from Canvas by its Canvas ID number
+	 * @param options Options class containing required and optional parameters for this API call
+	 * @return The DiscussionTopic returned by Canvas or an empty Optional
+	 * @throws IOException When there is an error communicating with Canvas
+	 */
 	Optional<DiscussionTopic> getSingleDiscussionTopic(GetSingleDiscussionTopicOptions options) throws IOException;
 
+	/**
+	 * Retrieve a list of Discussion Topics associated with a course
+	 * @param options Options class containing required and optional parameters for this API call
+	 * @return List of DiscussionTopics in the requested course
+	 * @throws IOException When there is an error communicating with Canvas
+	 */
 	List<DiscussionTopic> listCourseDiscussionTopics(ListCourseDiscussionTopicsOptions options) throws IOException;
 
 }
