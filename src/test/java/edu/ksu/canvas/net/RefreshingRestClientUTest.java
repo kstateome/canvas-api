5d3deb2c1e1d79edf908da0552cb8d012e3ca83b
David Malia <althor@ksu.edu>
2026-05-01 16:19:51 -0500

    Fixed failing tests due to mockito upgrade enabled dependency convergence check, and dependency-check on builds. Resolved issue found by dependency convergence check.

--- src/test/java/edu/ksu/canvas/net/RefreshingRestClientUTest.java
+++ src/test/java/edu/ksu/canvas/net/RefreshingRestClientUTest.java
@@ -7,17 +7,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyMap;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
